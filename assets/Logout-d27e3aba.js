cb431ce77d4ffed939cffa1cd3c293fd3f6374ab
TingHuang222324 <t99635@gmail.com>
2023-09-20 14:30:04 +0800

    feat: static page

---
+++ assets/Logout-d27e3aba.js
@@ -0,0 +1,1 @@
+import{ae as t,u as r,af as s}from"./index-9444415c.js";const n={__name:"Logout",setup(u){const o=t(),e=r();return s(()=>{e.clearToken(),o.push({name:"Login"})}),()=>{}}};export{n as default};
