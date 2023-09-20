c71f36b46da169da3e6f662c613c0c765666d8ad
TingHuang222324 <t99635@gmail.com>
2023-09-20 11:53:06 +0800

    fix base path

--- assets/Logout-886af27d.js
+++
@@ -1,1 +0,0 @@
-import{ac as t,u as r,ad as s}from"./index-86c02128.js";const n={__name:"Logout",setup(u){const o=t(),e=r();return s(()=>{e.clearToken(),o.push({name:"Login"})}),()=>{}}};export{n as default};
