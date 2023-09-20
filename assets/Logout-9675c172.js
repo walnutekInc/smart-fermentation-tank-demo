6eed3c90313ae9c45633720f368f5e5c66dd7acc
TingHuang222324 <t99635@gmail.com>
2023-09-20 12:02:13 +0800

    fix github page sanitizeFileName

--- assets/Logout-9675c172.js
+++
@@ -1,1 +0,0 @@
-import{ac as t,u as r,ad as s}from"./index-e02b5bf8.js";const n={__name:"Logout",setup(u){const o=t(),e=r();return s(()=>{e.clearToken(),o.push({name:"Login"})}),()=>{}}};export{n as default};
---
+++ assets/Logout-d72cb165.js
@@ -0,0 +1,1 @@
+import{ac as t,u as r,ad as s}from"./index-2ed5d23e.js";const n={__name:"Logout",setup(u){const o=t(),e=r();return s(()=>{e.clearToken(),o.push({name:"Login"})}),()=>{}}};export{n as default};
