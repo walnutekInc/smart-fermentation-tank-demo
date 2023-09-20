4f133ebc18d0936d4466a5609340d91b63100c75
TingHuang222324 <t99635@gmail.com>
2023-09-20 14:32:53 +0800

    fix footer name

---
+++ assets/Logout-21e8b87c.js
@@ -0,0 +1,1 @@
+import{ae as t,u as r,af as s}from"./index-29d56d19.js";const n={__name:"Logout",setup(u){const o=t(),e=r();return s(()=>{e.clearToken(),o.push({name:"Login"})}),()=>{}}};export{n as default};
--- assets/Logout-d27e3aba.js
+++
@@ -1,1 +0,0 @@
-import{ae as t,u as r,af as s}from"./index-9444415c.js";const n={__name:"Logout",setup(u){const o=t(),e=r();return s(()=>{e.clearToken(),o.push({name:"Login"})}),()=>{}}};export{n as default};
