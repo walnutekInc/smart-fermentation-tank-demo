89b12976505d0ca497900ed495b98e62e20f745f
TingHuang222324 <t99635@gmail.com>
2023-10-23 11:57:57 +0800

    modify flow

---
+++ assets/Logout-23deae3f.js
@@ -0,0 +1,1 @@
+import{ah as t,u as r,L as s}from"./index-c112ead2.js";const n={__name:"Logout",setup(u){const o=t(),e=r();return s(()=>{e.clearToken(),o.push({name:"Login"})}),()=>{}}};export{n as default};
--- assets/Logout-f666216b.js
+++
@@ -1,1 +0,0 @@
-import{ai as t,u as r,K as s}from"./index-95fe6a68.js";const n={__name:"Logout",setup(u){const o=t(),e=r();return s(()=>{e.clearToken(),o.push({name:"Login"})}),()=>{}}};export{n as default};
