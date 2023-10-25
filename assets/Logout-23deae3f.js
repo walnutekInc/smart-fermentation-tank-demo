64cd15c2060d2c48d35a23f19b81c82e0e6e359f
TingHuang222324 <t99635@gmail.com>
2023-10-25 11:36:08 +0800

    logs demo data

--- assets/Logout-23deae3f.js
+++
@@ -1,1 +0,0 @@
-import{ah as t,u as r,L as s}from"./index-c112ead2.js";const n={__name:"Logout",setup(u){const o=t(),e=r();return s(()=>{e.clearToken(),o.push({name:"Login"})}),()=>{}}};export{n as default};
