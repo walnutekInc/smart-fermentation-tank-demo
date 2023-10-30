fdcb42c73f89d8b62bba36b5a7e2a35ceed27863
TingHuang222324 <t99635@gmail.com>
2023-10-30 11:56:48 +0800

    fix: change laborator width

--- assets/Logout-b982de8b.js
+++
@@ -1,1 +0,0 @@
-import{ah as t,u as r,L as s}from"./index-9e5ab6d3.js";const n={__name:"Logout",setup(u){const o=t(),e=r();return s(()=>{e.clearToken(),o.push({name:"Login"})}),()=>{}}};export{n as default};
